613b7ee6fec0fc5ebc3948301af3bbd94e112635
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-03-04 10:54:22 -0800

    readstream to chunk to logfile to string

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -2,11 +2,12 @@ const connection = new WebSocket("ws://localhost:3000");
 
 const logWindow = document.querySelector("#log-window");
 
-// assign function to the connection objects ononpen event handler property
+const filePath = document.getElementById("logFilePath");
+
 connection.onopen = () => {
-    connection.send("Hello from the client!");
+    filePath ? connection.send(filePath) : connection.send("Hello from the client!");
 };
 
 connection.onmessage = (e) => {
     logWindow.innerHTML += e.data;
-}
+};
