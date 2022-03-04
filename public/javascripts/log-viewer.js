7f78b8d15ed9fe3239ac12759508e9cffe9e1ba2
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-03-04 12:22:17 -0800

    logs are rendering to the UI

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,20 +1,19 @@
 const connection = new WebSocket("ws://localhost:3000");
 
-const logWindow = document.getElementById("log-window");
+const logWindow = document.querySelector("#log-window");
 
 const filePath = document.getElementById("logFilePath");
 
 connection.onopen = () => {
-    filePath ? connection.send(filePath) : connection.send("Hello from the client!");
+    filePath.value ? connection.send(filePath.value) : connection.send("Hello from the client!");
 };
 
-console.log("log-viewer.js is running");
 
-connection.onmessage = (e) => {
+connection.onmessage = (event) => {
     // logWindow.innerHTML += e.data;
     // replace all new lines with a horizontal rule on event.data
     // const logs = e.data.replace(/\n/g, "<hr>");
-    const logs = e.data.split("\n").join("<hr>");
+    const logs = event.data.split("\n").join("<hr>");
     logWindow.innerHTML = logs;
     console.log(logs)
 };
