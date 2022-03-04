b447d6dc6a14c2d23a9531ea4eaf599572e63ab7
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-03-03 17:31:18 -0800

    websocket-based log added to viewer window

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,1 +1,12 @@
 const connection = new WebSocket("ws://localhost:3000");
+
+const logWindow = document.querySelector("#log-window");
+
+// assign function to the connection objects ononpen event handler property
+connection.onopen = () => {
+    connection.send("Hello from the client!");
+};
+
+connection.onmessage = (e) => {
+    logWindow.innerHTML += e.data;
+}
