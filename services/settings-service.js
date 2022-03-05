a08e3cb54c0019cf9e8c99d6c80eb368aabe3d70
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-03-05 09:54:36 -0800

    implemented getSettings ()

--- services/settings-service.js
+++ services/settings-service.js
@@ -4,10 +4,21 @@ const path = require('path');
 const settingsFilePath = path.join(__dirname, '../json/settings.json');
 
 function getSettings() {
- const settingsData = fs.readFileSync(settingsFilePath);
+    const settingsData = fs.readFileSync(settingsFilePath);
+
+    return JSON.parse(settingsData);
 }
 
 function writeSettings(newSettings) {
+    const settingsJSON = JSON.stringify(newSettings, null, 2);
+    try {
+        fs.writeFileSync(settingsFilePath, settingsJSON);
+        return true;
+    }
+    catch {
+        return false;
+    }
+
 }
 
 function getDefaultDir() {
@@ -17,8 +28,8 @@ function isValidDir(dirPath) {
 }
 
 module.exports = {
-  getSettings,
-  writeSettings,
-  getDefaultDir,
-  isValidDir
+    getSettings,
+    writeSettings,
+    getDefaultDir,
+    isValidDir
 };
